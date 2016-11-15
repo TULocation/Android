b1d8387d5b8e25461a1de49de9ffea4318606e4f
jankowskirobert <jankowski.r@hotmail.com>
2016-11-15 22:28:49 +0100

    fix files

---
+++ app/src/main/java/com/app/skynet/tulocation/TULocationMain.java
@@ -0,0 +1,14 @@
+package com.app.skynet.tulocation;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class TULocationMain extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_tulocation_main);
+    }
+}
