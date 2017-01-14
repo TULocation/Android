6aa36d25ab937a13e42cdb28460086467c41784e
Nerond <nerond32@gmail.com>
2017-01-14 22:48:50 +0100

    dojebana lista w osobnym aktiwiti

---
+++ app/src/main/java/com/app/skynet/tulocation/list/AccessPoint.java
@@ -0,0 +1,8 @@
+package com.app.skynet.tulocation.list;
+
+/**
+ * Created by Nerond on 14-Jan-17.
+ */
+
+public class AccessPoint {
+}
---
+++ app/src/main/java/com/app/skynet/tulocation/list/TULocationList.java
@@ -0,0 +1,9 @@
+package com.app.skynet.tulocation.list;
+
+
+public class TULocationList implements List {
+    @Override
+    public AccessPoint getElement() {
+        return null;
+    }
+}
