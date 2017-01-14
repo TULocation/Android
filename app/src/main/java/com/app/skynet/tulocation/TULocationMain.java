1c88fb94e3bb86469093c66cc32a201413978d95
jankowskirobert <jankowski.r@hotmail.com>
2017-01-14 22:28:52 +0100

    Add navigation menu

--- app/src/main/java/com/app/skynet/tulocation/TULocationMain.java
+++ app/src/main/java/com/app/skynet/tulocation/TULocationMain.java
@@ -2,13 +2,39 @@ package com.app.skynet.tulocation;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class TULocationMain extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_tulocation_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainactivity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_ap_list:
+               //HANDLE CLICK
+                return true;
+            case R.id.action_map_view:
+                //HANDLE CLICK
+                return true;
+            case R.id.action_app_settings:
+                //HANDLE CLICK
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
