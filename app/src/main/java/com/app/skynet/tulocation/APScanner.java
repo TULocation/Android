662febf9b724bfc099c91251eb91a2e0daf6dc2e
Nerond <nerond32@gmail.com>
2017-02-06 18:08:10 +0100

    quickfix receiver[not compiling]

--- app/src/main/java/com/app/skynet/tulocation/APScanner.java
+++ app/src/main/java/com/app/skynet/tulocation/APScanner.java
@@ -5,7 +5,15 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import com.app.skynet.tulocation.list.APList;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,12 +29,7 @@ public class APScanner {
         wifiList =  new ArrayList<String>();
         mainWifiObj = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
         mainWifiObj.startScan();
-        receiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-
-            }
-        };
+        if (receiver == null) receiver = new TestReceiver();
         mContext.registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         scan();
     }
@@ -37,7 +40,7 @@ public class APScanner {
         //---
         globalAPList = apScanned;
     }
-    /*class TestReceiver extends BroadcastReceiver {
+    class TestReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             List<ScanResult> wifiTmp = mainWifiObj.getScanResults();
@@ -47,5 +50,5 @@ public class APScanner {
                 System.out.println("TEST: is "+scanResult.toString());
             }
         }
-    }*/
+    }
 }
