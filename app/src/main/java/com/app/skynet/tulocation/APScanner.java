a37b48374c4b1adb7a31fd23c3b4203be9f885b4
jankowskirobert <jankowski.r@hotmail.com>
2017-02-06 21:09:02 +0100

    Dla Leniwego Krzycha

--- app/src/main/java/com/app/skynet/tulocation/APScanner.java
+++ app/src/main/java/com/app/skynet/tulocation/APScanner.java
@@ -21,20 +21,24 @@ public class APScanner {
         mainWifiObj = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
         mainWifiObj.startScan();
         if (receiver == null) receiver = new TestReceiver();
+        mContext.registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
     }
     public void scan(){
-        apScanned.eraseAllAP();
-        mContext.registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        globalAPList = apScanned;
+        mainWifiObj.startScan();
+
     }
     class TestReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
+            apScanned.eraseAllAP();
             List<ScanResult> wifiTmp = mainWifiObj.getScanResults();
             for (ScanResult scanResult : wifiTmp) {
                 apScanned.addAP(scanResult.BSSID, scanResult.SSID,0,0,scanResult.level);
             }
             apScanned.addAP("testowa", "testowymak", 10, 15, 20.0);
+            globalAPList = apScanned;
+            System.out.println("SSSSS");
         }
+
     }
 }
