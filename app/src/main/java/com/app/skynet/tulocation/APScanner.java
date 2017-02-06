7f241ac9aa89fcab675032d22db0f85f7a328d04
Nerond <nerond32@gmail.com>
2017-02-07 00:25:40 +0100

    leczymy raka roberta

--- app/src/main/java/com/app/skynet/tulocation/APScanner.java
+++ app/src/main/java/com/app/skynet/tulocation/APScanner.java
@@ -10,35 +10,42 @@ import java.util.List;
 
 import static com.app.skynet.tulocation.TULocationMain.globalAPList;
 
-public class APScanner {
-    Context mContext;
-    WifiManager mainWifiObj;
-    BroadcastReceiver receiver;
-    APList apScanned;
+public class APScanner implements IAPScanner{
+    final Object signal = new Object();
+    private Context mContext;
+    private WifiManager mainWifiObj;
+    private BroadcastReceiver receiver;
+    private APList apScanned;
     public APScanner(Context mContext) {
         this.mContext = mContext;
+        init();
+    }
+    private void init(){
         apScanned =  new APList();
         mainWifiObj = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
         mainWifiObj.startScan();
         if (receiver == null) receiver = new TestReceiver();
         mContext.registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
     }
-    public void scan(){
+    @Override
+    public APList scan(){
         mainWifiObj.startScan();
-
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return apScanned;
     }
-    class TestReceiver extends BroadcastReceiver {
+    private class TestReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             apScanned.eraseAllAP();
             List<ScanResult> wifiTmp = mainWifiObj.getScanResults();
             for (ScanResult scanResult : wifiTmp) {
-                apScanned.addAP(scanResult.BSSID, scanResult.SSID,0,0,scanResult.level);
+                apScanned.addAP(scanResult.SSID, scanResult.BSSID,0,0,scanResult.level);
             }
-            apScanned.addAP("testowa", "testowymak", 10, 15, 20.0);
-            globalAPList = apScanned;
-            System.out.println("SSSSS");
-        }
 
+        }
     }
 }
