cbe6f889db22a6d76b0f2083bd8fca6fd8158b02
Nerond <nerond32@gmail.com>
2017-02-06 18:05:33 +0100

    cleaned mess a bit

--- app/src/main/java/com/app/skynet/tulocation/APScanner.java
+++ app/src/main/java/com/app/skynet/tulocation/APScanner.java
@@ -5,15 +5,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
 import com.app.skynet.tulocation.list.APList;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,8 +21,14 @@ public class APScanner {
         wifiList =  new ArrayList<String>();
         mainWifiObj = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
         mainWifiObj.startScan();
-        if (receiver == null) receiver = new TestReceiver();
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+            }
+        };
         mContext.registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        scan();
     }
     public void scan(){
         APList apScanned = new APList();
@@ -39,11 +37,9 @@ public class APScanner {
         //---
         globalAPList = apScanned;
     }
-    class TestReceiver extends BroadcastReceiver {
+    /*class TestReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            System.out.println(action);
             List<ScanResult> wifiTmp = mainWifiObj.getScanResults();
             wifiList.clear();
             for (ScanResult scanResult : wifiTmp) {
@@ -51,5 +47,5 @@ public class APScanner {
                 System.out.println("TEST: is "+scanResult.toString());
             }
         }
-    }
+    }*/
 }
